227af3f43426bad3fcc7457b362a0612c542b5d7
Evan Lee <evanmlee@berkeley.edu>
2020-04-27 14:14:24 -0700

    Finished record filtering in main

--- spec_subs_main.py
+++ spec_subs_main.py
@@ -1,27 +1,48 @@
-from SSutility import SSconfig, SSdirectory, SSerrors
+from SSutility import SSdirectory, SSerrors
 import sys, os
 
-def ss_acquisition(config, spec_list, tax_subet, gene_id_df, tax_table):
-    # import ODBquery
-    # import NCBIquery
-    # import aliasQuery
+def ss_acquisition(config, gene_id_df, tax_table):
+    """Uses gene_symbol column in gene_id_df to query OrthoDB and NCBI for input sequence data. Downloads alias
+    information for all symbols from GeneCards into alias_data.
+
+    :param config: configparser object with run params. See SSutility.SSconfig
+    :param gene_id_df: DataFrame containing Gene ID and symbol information. See SSutility.SSconfig
+    :param tax_table: OrthoDB taxonomy table filtered down to input species list in config[SpeciesFilePath].
+    See SSutility.SSconfig
+    :return: N/A
+    """
     from SSacquisition import ODBquery,NCBIquery,aliasQuery
+    print("===Record Data Acquisition====")
     gene_symbols = gene_id_df['gene_symbol']
     valid_queries, failed_queries = ODBquery.download_ODB_input(gene_symbols, tax_table, config)
     ags_mapped_df = NCBIquery.download_AGS_data(gene_id_df, config)
     aliasQuery.download_alias_data(gene_symbols, config)
 
-def ss_filter(config, spec_list, tax_subet, gene_id_df, tax_table):
+def ss_filter(config, tax_subset, gene_id_df):
+    """Record Filtering for all input data. Logs error and quality check output, generates final dataset fastas and
+    records.tsv in [run_dir]/output, skipping OrthoDB/NCBI query errors and files with no usable test species data.
+
+    :param config: configparser object, see SSutility.SSconfig
+    :param tax_subset: list of Taxonomy IDs to be included in filtered output, read from config.txt
+    [AnalysisODBTaxSubset] section. Can be configured to select amy subset of species from raw OrthoDB input.
+    :param gene_id_df: DataFrame with gene ID and symbol information, SSutility.SSconfig
+    :return: N/A. Writes filtered output files to [run_dir]/output subdirectories separated by gene symbol.
+    """
 
     from SSfilter import ODBfilter,NCBIfilter
-    run_name,errors_fpath= config['RUN']['RunName'],config['RUN']['ErrorsFilePath']
-    ncbi_taxid,ncbi_specname = config['NCBI']['NCBITaxID'],config['NCBI']['NCBITaxName']
-    odb_test_taxid = config['ODB']['ODBTestTaxID']
+    print("===Record Data Filtering===")
+    print("Sequence Data Error and Quality Check warnings for mouse/human/13LGS will be printed")
+    print("Error logs and QC can be found within run directory ")
+
     gene_symbols = gene_id_df['gene_symbol']
-    tax_subset = list(config['AnalysisODBTaxSubset'].keys())
-    taxid_dict = {ncbi_specname: ncbi_taxid}
+    run_name,errors_fname,qc_fname= [config['RUN'][key] for key in ['RunName','ErrorsFileName','QCFileName']]
+    errors_fpath = "{0}/{1}".format(run_name,errors_fname)
+    qc_fpath = "{0}/{1}".format(run_name,qc_fname)
 
     check_errors,errors_df = SSerrors.load_errors(errors_fpath)
+    check_qc, qc_df = SSerrors.load_errors(qc_fpath)
+    qc_symbols = []
+
     for symbol in gene_symbols:
         out_records_fpath = "{0}/output/{1}/{1}_records.tsv".format(run_name, symbol)
         if not os.path.exists(out_records_fpath) or config['RUN'].getboolean('OverwriteFilter'):
@@ -30,22 +51,27 @@ def ss_filter(config, spec_list, tax_subet, gene_id_df, tax_table):
                 ncbi_error = (symbol in errors_df.loc[errors_df['error_type'] == "NCBIQueryError", 'gene_symbol'].unique())
                 if odb_error or ncbi_error:
                     continue
+                elif symbol in errors_df.loc[errors_df['error_type']=="SequenceDataError",'gene_symbol'].unique():
+                    SSerrors.print_errors(errors_df,symbol,error_type="SequenceDataError")
+                    continue
             try:
-                odb_fpath = "{0}/input/ODB/{1}.fasta".format(run_name,symbol)
-                ncbi_fpath = "{0}/input/NCBI/{1}/{2}.fasta".format(run_name,ncbi_taxid,symbol)
-                results = ODBfilter.process_input(symbol, config, tax_subset)
-                final_odb, em_df, am_df = results['final_df'], results['em_df'], results['am_df']
-                final_combined = NCBIfilter.select_NCBI_record(odb_fpath, ncbi_fpath, taxid_dict,
-                                                               final_odb, [odb_test_taxid])
-                comb_proc = NCBIfilter.combined_records_processing(config, am_df, em_df, final_combined, symbol)
+                NCBIfilter.final_combined_input(config,symbol,tax_subset)
             except SSerrors.SequenceDataError as e:
                 continue
+        #Write QC output for previously filtered results (ie out_records_fpath exists)
+        elif check_qc and symbol in qc_df['gene_symbol'].unique():
+            qc_symbols.append(symbol)
+    print("==Previosuly cached QC==")
+    for qc_symbol in qc_symbols:
+        for i,row in qc_df.loc[qc_df['gene_symbol']==qc_symbol,:].iterrows():
+            print("{0}\t{1}".format(row.values[0],row.values[1]))
+
 
 def main():
     import SSutility
-    from SSutility import config, spec_list, tax_subet, gene_id_df, tax_table
-    # ss_acquisition(config, spec_list, tax_subet, gene_id_df, tax_table)
-    ss_filter(config, spec_list, tax_subet, gene_id_df, tax_table)
+    from SSutility import config, tax_subset, gene_id_df, tax_table
+    ss_acquisition(config, gene_id_df, tax_table)
+    ss_filter(config, tax_subset, gene_id_df)
 
 if __name__ == '__main__':
     main()
